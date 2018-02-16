ba4249af3e9e450691f5d20b66a15cb1fdb032bb
Praveen Kumar Mallela <pmallela2@applabs.com>
2018-02-16 16:47:10 +0530

    fun

--- src/guru/Assignment2.java
+++ src/guru/Assignment2.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import junit.framework.Assert;
+
 
 public class Assignment2 {
 
